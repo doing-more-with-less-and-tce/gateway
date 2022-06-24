d966711b8e68f4a9be5100eccef144897a5eed78
dashaun <dashaun@dashaun.com>
2022-06-23 21:12:45 -0500

    redis

--- src/main/java/dev/dashaun/gateway/Application.java
+++ src/main/java/dev/dashaun/gateway/Application.java
@@ -2,7 +2,9 @@ package dev.dashaun.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
